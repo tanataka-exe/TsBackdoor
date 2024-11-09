9a7b17b29c19d24d085d5ee29d348e0f4e8dd4f8
Tanaka Takayuki <tanataka-exe@gmail.com>
2024-11-09 23:42:39 +0900

    Add License Terms of GPLv3

--- make-rss.php
+++ make-rss.php
@@ -1,4 +1,20 @@
 <?php
+    //    TsBackdoor - a poor static site generator for my own use.
+    //    Copyright (C) 2024  Tanaka Takayuki
+    //
+    //    This program is free software: you can redistribute it and/or modify
+    //    it under the terms of the GNU General Public License as published by
+    //    the Free Software Foundation, either version 3 of the License, or
+    //    (at your option) any later version.
+    //
+    //    This program is distributed in the hope that it will be useful,
+    //    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    //    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    //    GNU General Public License for more details.
+    //
+    //    You should have received a copy of the GNU General Public License
+    //    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
     $stdin = fgets(STDIN);
     $sitename = $argv[1];
     $data = json_decode($stdin, true);
